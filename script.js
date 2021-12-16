5d688b86190ba89bf98803dfe38445e483993529
RadiantBlue223 <Landsberger_Mike@student.mahoningctc.com>
2021-12-16 17:14:01 -0500

    Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function myFunction(){
+
+    let vNum = Math.floor(Math.random() * 10) + 1;
+    console.log(vNum);
+
+    if( vNum == 1 ){
+        document.getElementById("answer").innerHTML = ("Without Anime Girls Life Would Not Be Poggers")
+    }else if( vNum == 2){
+        document.getElementById("answer").innerHTML = ("To Infinity And Beyond!!!")
+    }else if( vNum == 3){
+        document.getElementById("answer").innerHTML = ("Commit Arson")
+    }else if( vNum == 4){
+        document.getElementById("answer").innerHTML = ("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    }else if( vNum == 5){
+        document.getElementById("answer").innerHTML = ("(Redacted)")
+    }else if( vNum == 6){
+        document.getElementById("answer").innerHTML = ("Ur Mom")
+    }else if( vNum == 7){
+        document.getElementById("answer").innerHTML = ("No")
+    }else if( vNum == 8){
+        document.getElementById("answer").innerHTML = ("Funny")
+    }else if( vNum == 9){
+        document.getElementById("answer").innerHTML = ("Cat   Fish")
+    }else if( vNum == 10){
+        document.getElementById("answer").innerHTML = ("Chris Pratt is Mario")
+    }
+
+}
+
